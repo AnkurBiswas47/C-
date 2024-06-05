8fd6c6a7d1e5d6dd431cad74eaa67ea0527b7caa
Rinji47 <ankur.biswas.s24@icp.edu.np>
2024-06-05 09:31:06 +0545

    First push

---
+++ code.cs
@@ -0,0 +1,38 @@
+﻿namespace ye
+{
+
+    public class Code
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your age");
+            String Email = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(emailValidation(Email));
+        }
+
+        public static string emailValidation(string eMail)
+
+        {
+            if (!char.IsDigit(eMail[0]) && eMail.Contains("@") && eMail.EndsWith(".com"))
+            {
+
+                return "Email validation sucessful";
+            }
+            else
+            {
+                if (char.IsDigit(eMail[0]))
+                {
+                    Console.WriteLine("Please start your email with an alphabate");
+                } else if (!eMail.Contains("@"))
+                {
+                    Console.WriteLine("Please add @ in your email");
+                } else if (!eMail.EndsWith(".com"))
+                {
+                    Console.WriteLine($"Please put {".com"} at the end of your email.");
+                }
+                
+                return "Email validation failed";
+            }
+        }
+    }
+}
